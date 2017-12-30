1d47eae8136a937c9b2964f28d49e6d3f99399a7
Philipp Palutin <russdragon9000@gmail.com>
2017-12-30 23:03:03 +0300

    limerickgen.lua: added functions for work with dictionaries

--- limerickgen.lua
+++ limerickgen.lua
@@ -9,7 +9,7 @@ local pn = py.import('pronouncing')
 
 local lim_sizes =
 {
-  [1] = 9,
+  [1] = 8, -- 8 because we count 2 syllables in 'there'
   [2] = 8,
   [3] = 6,
   [4] = 6,
@@ -29,7 +29,8 @@ end
 local string_syllable_count = function(str)
   assert(str, 'string_syllable_count: str must be passed')
 
-  if type(str) == 'table' then
+  -- HACK. CHANGE LATER
+  if type(str) == 'table' then 
     str = table.concat(str, ' ') or ''
   end
 
@@ -77,6 +78,55 @@ end
 
 -------------------------------------------------------------------------------
 
+-- Use only with dictionaries like obj_rhymes/place_rhymes, first word must be
+-- main and all the rest is rhyming to it
+local get_rhyming_pair = function(dictionary, sylls, stress_pos)
+  -- Here must be assertions
+
+  local ok_sets = {}
+
+  for _, set in pairs(dictionary) do
+    local ok_rhymes = {}
+    local main = set[1]
+
+    if check_word_size(main, sylls, stress_pos) then
+      for j = 2, #set do
+        if comapre_word_sizes(main, set[j]) then
+          table_insert(ok_rhymes, set[j])
+        end
+      end
+    end
+
+    if #ok_rhymes > 0 then
+      local index = #ok_sets+1
+      ok_sets[index] = {}
+      ok_sets[index][1] = main
+
+      for _, v in pairs(ok_rhymes) do
+        table_insert(ok_sets[index], v)
+      end
+    end
+  end
+
+  local set = ok_sets[math_random(1, #ok_sets)]
+
+  return set[1], set[math_random(2, #set)]
+end
+
+-- Use only for plain dictionaries like adjs, descriptions, nouns, etc
+local get_random_word = function(dictionary, sylls, stress_pos)
+  local ok_elems = {}
+  for _, elem in pairs(dictionary) do
+    if check_word_size(elem, sylls, stress_pos) then
+      table_insert(ok_elems, elem)
+    end
+  end
+
+  return ok_elems[math_random(1, #ok_elems)]
+end
+
+-------------------------------------------------------------------------------
+
 local create_limerick = function()
   local pattern =
   {
@@ -89,141 +139,63 @@ local create_limerick = function()
 
   local occupations = JSON_to_table('dict/occupations.json')
   local nouns = JSON_to_table('dict/one_syl_nouns.json')
-
-  local places_rhymes = JSON_to_table('dict/place_rhymes.json')
-
   local descr = JSON_to_table('dict/descriptions.json')
 
+  local places_rhymes = JSON_to_table('dict/place_rhymes.json')
   local objects_rhymes = JSON_to_table('dict/obj_rhymes.json')
-  local objects = JSON_to_table('dict/objs.json')
 
   do
-    local who_article = ''
-    local who_syl = 0
-    local who_stress = 0
+    local who_syl
+    local who_stress
 
     -- 1 – a/an young/old (2syl), 2 – a/an (3syl)
     if math_random(1, 2) == 1 then
+      if math_random(1, 2) == 1 then pattern[1][3] = 'an Old' else pattern[1][3] = 'a Young' end
+
       who_syl = 2
       who_stress = 1
-
-      if math_random(1, 2) == 1 then who_article = 'an Old' else who_article = 'a Young' end
     else
       who_syl = 3
       who_stress = 2
-
-      who_article = nil
     end
 
-    local ok_occup = {}
-    for _, occ in pairs(occupations) do
-      if check_word_size(occ, who_syl, who_stress) then
-        occ = occ:gsub('^%l', string.upper)
-        local vowels = 'AEIOU'
+    local who = get_random_word(occupations, who_syl, who_stress):gsub('^%l', string.upper)
+    pattern[1][4] = who
+    pattern[5][4] = pattern[1][4]
 
-        local article
-        if not who_article then
-          if vowels:find(occ:match('.')) then article = 'an' else article = 'a' end
-        end
-        article = article or who_article
-
-        table_insert(ok_occup, {[1] = article, [2] = occ})
-        article = nil -- HACK. FIX LATER
-      end
+    if pattern[1][3] == '' then
+      local vowels = 'AEIOU'
+      
+      if vowels:find(who:match('.')) then pattern[1][3] = 'an' else pattern[1][3] = 'a' end
     end
-
-    local occ_obj = ok_occup[math_random(1, #ok_occup)]
-    pattern[1][3] = occ_obj[1]
-    pattern[1][4] = occ_obj[2]
 
     pattern[5][3] = pattern[1][3]:match(' (.+)') or '' -- HACK, FIX LATER
-    pattern[5][4] = pattern[1][4]
   end
 
   do
-    local place_syl = lim_sizes[1] - 8
+    local place_syl = count_free_syls(pattern[1], lim_sizes[1])
     local place_stress = 1
-    local ok_places = {}
 
-    for _, place in pairs(places_rhymes) do
-      local ok_adjs = {}
-      local place_name = place[1]
+    local place, adj = get_rhyming_pair(places_rhymes, place_syl, place_stress)
 
-      if check_word_size(place_name, place_syl, place_stress) then
-        for j = 2, #place do
-          if comapre_word_sizes(place_name, place[j]) then
-            table_insert(ok_adjs, place[j])
-          end
-        end
-      end
-
-      if #ok_adjs > 0 then
-        local index = #ok_places+1
-        ok_places[index] = {}
-        ok_places[index][1] = place_name
-
-        for _, v in pairs(ok_adjs) do
-          table_insert(ok_places[index], v)
-        end
-      end
-    end
-
-    local place_obj = ok_places[math_random(1, #ok_places)]
-    pattern[1][6] = place_obj[1]
+    pattern[1][6] = place
+    pattern[2][6] = adj
     pattern[5][6] = pattern[1][6]
-    pattern[2][6] = place_obj[math_random(2, #place_obj)]
   end
 
   do
-    -- TODO: Make a dictionary to remove while true do
-    while true do
-      local adj = descr[math_random(1, #descr)]
-
-      if pattern[2][4] == '' then
-        if check_word_size(adj, count_free_syls(pattern[2], lim_sizes[2]), 1) then
-          pattern[2][4] = adj
-        end
-      elseif pattern[5][2] == '' then
-        if check_word_size(adj, count_free_syls(pattern[5], lim_sizes[5]), 2) then
-          pattern[5][2] = adj
-        end
-      end
-
-      if pattern[2][4] ~= '' and pattern[5][2] ~= '' then break end
-    end
+    pattern[2][4] = get_random_word(descr, count_free_syls(pattern[2], lim_sizes[2]), 1)
+    pattern[5][2] = get_random_word(descr, count_free_syls(pattern[5], lim_sizes[5]), 2)
   end
 
   do
-    local obj_syl = lim_sizes[3] - 4
+    local obj_syl = count_free_syls(pattern[3], lim_sizes[3])
     local obj_stress = 1
-    local ok_objs = {}
 
-    for _, obj in pairs(objects_rhymes) do
-      local ok_objs_rhymes = {}
-      local obj_name = obj[1]
+    local obj, obj_rhyme = get_rhyming_pair(objects_rhymes, obj_syl, obj_stress)
 
-      if check_word_size(obj_name, obj_syl, obj_stress) then
-        for j = 2, #obj do
-          if comapre_word_sizes(obj_name, obj[j]) then
-            table_insert(ok_objs_rhymes, obj[j])
-          end
-        end
-      end
-
-      if #ok_objs_rhymes > 0 then
-        local index = #ok_objs+1
-        ok_objs[index] = {}
-        ok_objs[index][1] = obj_name
-
-        for _, v in pairs(ok_objs_rhymes) do
-          table_insert(ok_objs[index], v)
-        end
-      end
-    end
-
-    local temp = ok_objs[math_random(1, #ok_objs)] -- HACK. FIX LATER
-    pattern[3][5] = temp[1]
-    pattern[4][4] = temp[math_random(2, #temp)]
+    pattern[3][5] = obj
+    pattern[4][4] = obj_rhyme
   end
   
   pattern[4][2] = nouns[math.random(1, #nouns)]
@@ -240,8 +212,8 @@ end
 
 math_randomseed(os_time())
 
-local iters = select(1, ...) or 1
+local iters = select(1, ...) or 10
 for i = 1, iters do
   local lim = table_concat(create_limerick(), ',\n') .. '.'
-  print(lim)
+  print(lim, '\n')
 end
