42c604a7e4e04bd92073ac3d4b1352556c0fa7a1
Philipp Palutin <russdragon9000@gmail.com>
2018-03-06 21:30:16 +0300

    limerickgen.lua: added mutation per word and command line arguments

--- limerickgen.lua
+++ limerickgen.lua
@@ -1,3 +1,5 @@
+require 'lua-nucleo'
+
 local pairs, select, math_randomseed, math_random, table_insert, table_concat,
       os_time, io_open, type, string_lower
     = pairs, select, math.randomseed, math.random, table.insert, table.concat,
@@ -6,6 +8,11 @@ local pairs, select, math_randomseed, math_random, table_insert, table_concat,
 local json = require('cjson')
 local py = require('python')
 local pn = py.import('pronouncing')
+
+local split_by_char = import 'lua-nucleo/string'
+{
+  'split_by_char';
+}
 
 -------------------------------------------------------------------------------
 
@@ -17,11 +24,13 @@ local JSON_to_table = function(path)
   return json.decode(js)
 end
 
+-------------------------------------------------------------------------------
+
 local string_syllable_count = function(str)
   assert(str, 'string_syllable_count: str must be passed')
 
   -- HACK. CHANGE LATER
-  if type(str) == 'table' then 
+  if type(str) == 'table' then
     str = table.concat(str, ' ') or ''
   end
 
@@ -116,6 +125,22 @@ local get_random_word = function(dictionary, sylls, stress_pos)
   return ok_elems[math_random(1, #ok_elems)]
 end
 
+
+-------------------------------------------------------------------------------
+
+local parse_limerick = function(str)
+  local pattern = {}
+
+  for k, line in pairs(split_by_char(str, '\n')) do
+    pattern[k] = {}
+    for word in line:gmatch('%w+') do
+      table_insert(pattern[k], word)
+    end
+  end
+
+  return pattern
+end
+
 -------------------------------------------------------------------------------
 
 local occupations = JSON_to_table('dict/occupations.json')
@@ -136,62 +161,114 @@ local lim_sizes =
 
 local make_limerick
 do
-  local generate = function(self)
-    do
-      local who_syl
-      local who_stress
+  local add_occupation = function(pattern)
+    pattern[1][3] = ''
+    pattern[1][4] = ''
+    pattern[5][3] = ''
+    pattern[5][4] = ''
 
-      -- 1 – a/an young/old (2syl), 2 – a/an (3syl)
+    local who_syl
+    local who_stress
+
+    -- 1 – a/an young/old (2syl), 2 – a/an (3syl)
+    if math_random(1, 2) == 1 then
       if math_random(1, 2) == 1 then
-        if math_random(1, 2) == 1 then self.pattern_[1][3] = 'an Old' else self.pattern_[1][3] = 'a Young' end
-
-        who_syl = 2
-        who_stress = 1
+        pattern[1][3] = 'an'
+        pattern[1][4] = 'Old'
       else
-        who_syl = 3
-        who_stress = 2
+        pattern[1][3] = 'a'
+        pattern[1][4] = 'Young'
       end
 
-      local who = get_random_word(occupations, who_syl, who_stress):gsub('^%l', string.upper)
-      self.pattern_[1][4] = who
-      self.pattern_[5][4] = self.pattern_[1][4]
+      who_syl = 2
+      who_stress = 1
+    else
+      who_syl = 3
+      who_stress = 2
+    end
 
-      if self.pattern_[1][3] == '' then
-        local vowels = 'AEIOU'
-        
-        if vowels:find(who:match('.')) then self.pattern_[1][3] = 'an' else self.pattern_[1][3] = 'a' end
+    local who = get_random_word(occupations, who_syl, who_stress):gsub('^%l', string.upper)
+    pattern[1][5] = who
+    pattern[5][4] = pattern[1][5]
+
+    if pattern[1][3] == '' then
+      local vowels = 'AEIOU'
+
+      if vowels:find(who:match('.')) then
+        pattern[1][3] = 'an'
+      else
+        pattern[1][3] = 'a'
       end
-
-      self.pattern_[5][3] = self.pattern_[1][3]:match(' (.+)') or '' -- HACK, FIX LATER
     end
 
-    do
-      local place_syl = count_free_syls(self.pattern_[1], lim_sizes[1])
-      local place_stress = 1
+    pattern[5][3] = pattern[1][4]
+  end
 
-      local place, adj = get_rhyming_pair(places_rhymes, place_syl, place_stress)
+  local add_place = function(pattern)
+    pattern[1][7] = ''
+    pattern[2][6] = ''
+    pattern[5][6] = ''
 
-      self.pattern_[1][6] = place
-      self.pattern_[2][6] = adj
-      self.pattern_[5][6] = self.pattern_[1][6]
+    local place_syl = count_free_syls(pattern[1], lim_sizes[1])
+    local place_stress = 1
+
+    local place, adj = get_rhyming_pair(places_rhymes, place_syl, place_stress)
+
+    pattern[1][7] = place
+    pattern[2][6] = adj
+    pattern[5][6] = pattern[1][7]
+  end
+
+  local add_descriptions = function(pattern)
+    pattern[2][4] = ''
+    pattern[5][2] = ''
+
+    pattern[2][4] = get_random_word(descr, count_free_syls(pattern[2], lim_sizes[2]), 1)
+    pattern[5][2] = get_random_word(descr, count_free_syls(pattern[5], lim_sizes[5]), 2)
+  end
+
+  local add_objects = function(pattern)
+    pattern[3][5] = ''
+    pattern[4][4] = ''
+
+    local obj_syl = count_free_syls(pattern[3], lim_sizes[3])
+    local obj_stress = 1
+
+    local obj, obj_rhyme = get_rhyming_pair(objects_rhymes, obj_syl, obj_stress)
+
+    pattern[3][5] = obj
+    pattern[4][4] = obj_rhyme
+  end
+
+  local add_noun = function(pattern)
+    pattern[4][2] = ''
+    pattern[4][2] = nouns[math.random(1, #nouns)]
+  end
+
+  local generate = function(self)
+    add_occupation(self.pattern_)
+    add_place(self.pattern_)
+    add_descriptions(self.pattern_)
+    add_objects(self.pattern_)
+    add_noun(self.pattern_)
+
+    return self
+  end
+
+  local mutate = function(self)
+    local change_pos = math.random(1, 5)
+
+    if change_pos == 1 then
+      add_occupation(self.pattern_)
+    elseif change_pos == 2 then
+      add_place(self.pattern_)
+    elseif change_pos == 3 then
+      add_descriptions(self.pattern_)
+    elseif change_pos == 4 then
+      add_objects(self.pattern_)
+    elseif change_pos == 5 then
+      add_noun(self.pattern_)
     end
-
-    do
-      self.pattern_[2][4] = get_random_word(descr, count_free_syls(self.pattern_[2], lim_sizes[2]), 1)
-      self.pattern_[5][2] = get_random_word(descr, count_free_syls(self.pattern_[5], lim_sizes[5]), 2)
-    end
-
-    do
-      local obj_syl = count_free_syls(self.pattern_[3], lim_sizes[3])
-      local obj_stress = 1
-
-      local obj, obj_rhyme = get_rhyming_pair(objects_rhymes, obj_syl, obj_stress)
-
-      self.pattern_[3][5] = obj
-      self.pattern_[4][4] = obj_rhyme
-    end
-    
-    self.pattern_[4][2] = nouns[math.random(1, #nouns)]
 
     return self
   end
@@ -205,15 +282,26 @@ do
     return table_concat(lim, '\n') .. '.'
   end
 
+  local set_pattern = function(self, str)
+    self.pattern_ = parse_limerick(str)
+  end
+
+  local get_pattern = function(self)
+    return self.pattern_
+  end
+
   make_limerick = function()
     return
     {
       generate = generate;
+      mutate = mutate;
       render = render;
+      set_pattern = set_pattern;
+      get_pattern = get_pattern;
 
       pattern_ =
       {
-        [1] = { 'There', 'was', '', '', 'of', '' },
+        [1] = { 'There', 'was', '', '', '', 'of', '' },
         [2] = { 'Whose', 'conduct', 'was', '', 'and', '' },
         [3] = { 'He', 'sat', 'on', 'the', '' },
         [4] = { 'Eating', '', 'and', '' },
@@ -227,8 +315,28 @@ end
 
 math_randomseed(os_time())
 
-local iters = select(1, ...) or 1
+local iters = select(1, ...) or 4
+local is_mutated = (select(2, ...) == '--mutate')
+local passed_lim = select(3, ...)
+
+if not is_mutated and passed_lim then
+  error('ERROR: you passed limerick but did not specify mutation flag')
+end
+
 local limerick = make_limerick()
+
+if passed_lim then
+  limerick:set_pattern(passed_lim)
+else
+  limerick:generate()
+end
+
 for i = 1, iters do
-  print(limerick:generate():render(), '\n')
+  io.write(limerick:render(), '\n\n')
+
+  if is_mutated then
+    limerick:mutate()
+  else
+    limerick:generate()
+  end
 end
